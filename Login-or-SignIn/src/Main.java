bf14b2635630e1e955307f75170732c3289528c1
Sandeep Singh <ssandeepssingh55@gmail.com>
2021-07-02 12:32:56 +0800

    cool

--- Login-or-SignIn/src/Main.java
+++ Login-or-SignIn/src/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -6,32 +7,43 @@ public class Main {
         public static void main(String[] args) {
             signUp();
         }
+
         static HashMap<String ,String> sandeep=new HashMap<>();
         public static  void signUp(){
             Scanner scanner=new Scanner(System.in);
             System.out.println("You are now signing up");
             System.out.println("Enter your email please");
             String email= scanner.nextLine();
-            while (!acceptedEmail(email)){
+            while (!acceptedEmail(email)) {
                 System.out.println("Your email is invalid please try again");
-                email=scanner.nextLine();
-                acceptedEmail(email);
-            }
-            if(sandeep.containsKey(email)){
-                System.out.println("Your email is already in our database");
-                signUpOrLogin();
-            }
-            else {
-                System.out.println("Enter your password please");
-                String password = scanner.nextLine();
-                sandeep.put(email, password);
-                System.out.println(sandeep);
-                login();
+                email = scanner.nextLine();
+                acceptedEmail(email); }
+            reenter(email);
+            if (sandeep.containsKey(email)) {
+                    System.out.println("Your email is already in our database");
+                    signUpOrLogin();
+                } else {
+                    System.out.println("Enter your password please");
+                    String password = scanner.nextLine();
+                    sandeep.put(email, password);
+                    System.out.println(sandeep);
+                    login();
+                } }
+        public static void reenter(String email){
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("Please reenter your email");
+            String second=scanner.nextLine();
+            if(!second.equals(email)){
+                System.out.println("Your emails are not the same please re-enter it");
+                signUp();
             }
         }
+
+        static  int count=0;
+
         public static void login(){
-            System.out.println("You are now logging in");
             Scanner scanner=new Scanner(System.in);
+            System.out.println("You are now logging in");
             System.out.println("Enter your email please");
             String username=scanner.nextLine();
             System.out.println("Enter your password please");
@@ -39,11 +51,11 @@ public class Main {
             if(sandeep.containsKey(username)&& sandeep.get(username).equals(password)){
                 System.out.println("Entering...");
             }else {
-                System.out.println("Sorry your email and password is not in our database");
-                signUpOrLogin();
-
+                emailNotThere();
             }
+
         }
+
         public static void signUpOrLogin(){
             Scanner scanner=new Scanner(System.in);
             System.out.println("Would you like to sign up or Login?");
@@ -57,6 +69,9 @@ public class Main {
             }
 
         }
+        public static void cantEnter(){
+            System.out.println("You have exceeded the number of login attempts");
+        }
         public static boolean acceptedEmail (String email){
             char [] ss=email.toCharArray();
             for (int i = 0; i < email.length(); i++) {
@@ -65,6 +80,14 @@ public class Main {
                 }
 
             }return false;
+        }
+        public static void emailNotThere(){
+            System.out.println("Sorry your email and password is not in our database");
+            count++;
+            if(count>=3){
+                cantEnter();
+            }else {
+                signUpOrLogin();}
         }
 
 
